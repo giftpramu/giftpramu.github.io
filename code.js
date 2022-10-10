2f6915d5071471c1eb838f4e5b43707d41b64b32
Pramudi Gamage <110784195+giftpramu@users.noreply.github.com>
2022-10-10 08:01:19 +0530

    myweb

---
+++ code.js
@@ -0,0 +1,8 @@
+alert("This is a personal website");
+
+function onmouseover(){
+    document.getElementById("demo").style.color="yellow";
+}
+function onmouseout(){
+    document.getElementById("demo").style.color="black";
+}
